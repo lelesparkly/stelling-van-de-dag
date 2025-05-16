1f5ef5cf49159a923e91ac89479fb940a6baaf84
lelesparkly <lmendesdias07@gmail.com>
2025-05-16 09:58:14 +0200

    Update index.js

--- stelling-van-de-dag-fixed/src/index.js
+++ stelling-van-de-dag-fixed/src/index.js
@@ -4,6 +4,3 @@ import ReactDOM from "react-dom/client";
 import CalendarStatement from "./CalendarStatement";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<CalendarStatement />);
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
